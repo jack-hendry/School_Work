9bc6f8eac3c9c2214be8beb0cc04c6e5e65bdf99
jack-hendry <52470464+jack-hendry@users.noreply.github.com>
2021-03-08 21:57:49 -0500

    Safety

--- year4/SYSC4001/A1/test1/test1.bat
+++ year4/SYSC4001/A1/test1/test1.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-Start "SYSC_4001_A1" "C:\Users\jacks\Google Drive\school_GD\year-4\sem2\SYSC 4001\assignments\SYSC_4001_A1\test1\SYSC_4001_A1.exe"
+Start "SYSC_4001_A1" "Where folder is located/SYSC_4001_A1.exe"
